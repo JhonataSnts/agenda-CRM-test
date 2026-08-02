fcdd3410c18c22217be6c8aa1f7312196e8a5143
Jonatas Santos <jonatasskt18@gmail.com>
2026-08-02 00:08:40 -0300

    delete.php adicionado

---
+++ delete.php
@@ -0,0 +1,15 @@
+﻿<?php
+
+require_once 'config/database.php';
+
+$id = (int) ($_GET['id'] ?? 0);
+
+if ($id <= 0) {
+    die('Contato inválido.');
+}
+
+$stmt = $pdo->prepare('DELETE FROM contatos WHERE id = :id');
+$stmt->execute([':id' => $id]);
+
+header('Location: index.php');
+exit;
